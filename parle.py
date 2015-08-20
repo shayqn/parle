513f48e59785d3893728b934f4add716bd962540
Shay Neufeld <shayneufeld@gmail.com>
2015-08-20 16:21:24 -0400

    initial commit

---
+++ parle.py
@@ -0,0 +1,45 @@
+import sys
+from flask import Flask
+from flask import render_template
+import psycopg2
+
+reload(sys)
+sys.setdefaultencoding("utf-8")
+
+conn = psycopg2.connect("dbname=postgres user=postgres password = roastb33f")
+
+# Open a cursor to perform database operations
+cur = conn.cursor()
+
+app = Flask(__name__)
+
+cur.execute("SELECT name,id FROM core_politician WHERE gender NOT LIKE '' ORDER BY name_family")
+nameID = cur.fetchmany(10)
+
+def getPolInfo(politician_id):
+    cur.execute("SELECT votequestion_id \
+FROM bills_membervote \
+WHERE politician_id = '" + str(politician_id) + "' AND \
+votequestion_id IN \
+(SELECT id FROM bills_votequestion \
+WHERE description_en LIKE ('%Bill be now read a third time and do pass%')) \
+ORDER BY votequestion_id")
+
+    memberVoteID = cur.fetchall()
+    print memberVoteID
+    return memberVoteID
+
+print nameID
+
+@app.route('/')
+def def_index():
+    return render_template('index.html',nameID = nameID)
+
+@app.route('/<id>')
+def politican_page(id):
+    memberVoteID = getPolInfo(id)
+    return render_template('pol.html',memberVoteID = memberVoteID)
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
