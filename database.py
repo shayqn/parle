cfe0f8b3700ec47ba2c0498eb7a1a9d408c9e712
Braden Neufeld <bneufeld@hmc.edu>
2015-09-03 15:55:16 -0700

    Cleanup and reorganization of all functions and modules, integration with React frontend, plus lots of comments.

---
+++ database.py
@@ -0,0 +1,22 @@
+"""
+Handles the database connection and cursor creation.
+We put it in its own module so it can be easily accessed from all others simply using 'import cursor from database'
+or 'import db from database' if you want direct access to the connection.
+
+Modules:
+    psycopg2: for PostgreSQL interaction
+    pyscopg2.extras: for RealDictCursor to make jsonifying results easier
+    settings: for database credentials
+"""
+import psycopg2
+from psycopg2.extras import RealDictCursor
+from settings import DBHOST, DBNAME, DBUSER, DBPASSWORD
+
+# Open our database connection
+# uses the 'keyword arguments' style - see http://initd.org/psycopg/docs/module.html#psycopg2.connect
+db = psycopg2.connect(host=DBHOST, database=DBNAME, user=DBUSER, password=DBPASSWORD)
+
+# Open a cursor to perform database operations
+# We use the psycopg2.extra RealDictCursor to get results in a Python dictionary instead of tuples
+# This is useful because it preserves a {key1: value2, key2: value2} relationship as opposed to (value1, value2,)
+cursor = db.cursor(cursor_factory=RealDictCursor)
