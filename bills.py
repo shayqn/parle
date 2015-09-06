e3b20d16abb6df003468c97f52f8fc72565677f7
Shay Neufeld <shayneufeld@gmail.com>
2015-09-06 16:13:04 -0400

    added queries to get the votes and bill information in politician.py and bills.py respectively.

--- bills.py
+++ bills.py
@@ -12,14 +12,14 @@ from database import cursor
 from flask import jsonify
 
 
-def get_bill_json(bill_id, cursor=cursor):
+def get_bill_json(votequestion_id, cursor=cursor):
     """
     Returns the JSON data for information on a single specific bill within a politician's profile.
     Is requested as the <Profile /> component is built in React.
 
     Related React.js component: <Bill />
 
-    :param bill_id: id of the single bill requested
+    :param votequestion_id: votequestion_id that corresponds to the single bill requested
     :param cursor: current database cursor - no need to supply this or change this from the default
     :return: a jsonify'd dictionary with all the desired bill information
     """
@@ -27,12 +27,20 @@ def get_bill_json(bill_id, cursor=cursor):
     # Build our query with all the desired relevant info we want to display about bills.
     # Add any parameters using (%s) or (%(name)s). More information below.
     query = (
-        " "
+        "SELECT number,name_en,law,short_title_en,institution "
+        "FROM bills_bill "
+        "WHERE id = "
+        "("
+            "SELECT bill_id "
+            "FROM bills_votequestion "
+            "WHERE id = (%s)"
+        ")"
     )
+
     # If any parameters are used in the above query, insert them in the parameters tuple
     # Insert parameters in the order used in the query unless using %(name)s placeholders
     # see http://initd.org/psycopg/docs/usage.html#passing-parameters-to-sql-queries for more information
-    parameters = (bill_id,)
+    parameters = (votequestion_id,)
 
     # Execute query
     cursor.execute(query, parameters)
