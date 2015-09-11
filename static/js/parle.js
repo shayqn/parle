a96478a4dcd04436be35cb2a8a698b251ab425ec
Braden Neufeld <bneufeld@hmc.edu>
2015-09-11 00:22:01 -0700

    bill display & search implementation

--- static/js/parle.js
+++ static/js/parle.js
@@ -8,44 +8,44 @@ var AppBox = React.createClass({
       profileClass: 'hide',
       profile: '',
       searching: false,
-      votes: []
+      votes: [],
+      retrievingVotes: false
     };
   },
   changeClass: function(object, event) {
     if (this.state.searchClass == 'full') {
-      console.log("full width");
       this.setState({
         searchClass : 'sidebar',
         profileClass : 'show',
-        profile : object
+        profile : object,
+        votes: [],
+        retrievingVotes: true
       });
       this.getPoliticianVotes(object.id);
     }
     else if (object.id == this.state.profile.id) {
-      console.log("active");
       this.setState({
         searchClass : 'full',
         profileClass: 'hide',
         profile : '',
-        votes: []
+        retrievingVotes: true
       });
       this.getPoliticianVotes(object.id);
     }
     else if (object) {
-      console.log("object else if");
       this.setState({
         profile : object,
-        votes: []
+        votes: [],
+        retrievingVotes: true
       });
       this.getPoliticianVotes(object.id);
     }
     else {
-      console.log("else");
       this.setState({
         searchClass : 'full',
         profileClass: 'hide',
         profile : '',
-        votes: []
+        retrievingVotes: false
       });
     }
   },
@@ -54,13 +54,20 @@ var AppBox = React.createClass({
         searchClass : 'full',
         profileClass: 'hide',
         profile: '',
-        votes: []
+        votes: [],
+        retrievingVotes: false
       });
   },
   onSearchChange: function(event) {
     this.setState({
       searching: true,
       searchValue: event.target.value
+    });
+  },
+  onBillSearchChange: function(event) {
+    this.setState({
+      billSearching: true,
+      billSearchValue: event.target.value
     });
   },
   render: function() {
@@ -80,28 +87,38 @@ var AppBox = React.createClass({
     else {
       var politicianList = this.state.politicians.slice(0,10);
     }
+    if (this.state.billSearching && this.state.billSearchValue) {
+      var regex = new RegExp(this.state.billSearchValue, "i");
+      var filteredList = this.state.votes.filter(function (vote) {
+        return vote.name_en.search(regex) > -1 || vote.number.search(regex) > -1 || vote.short_title_en.search(regex) > -1;
+      });
+      var voteList = filteredList;
+    }
+    else {
+      var voteList = this.state.votes;
+    }
+    var classes = 'box ' + this.state.searchClass;
     return (
-      <div className="box">
+      <div className={classes}>
         <SearchBox searchClass={this.state.searchClass} politicians={politicianList} changeClass={this.changeClass} onSearchChange={this.onSearchChange} profile={this.state.profile} />
-        <ProfileBox profileClass={this.state.profileClass} profile={this.state.profile} closeProfile={this.closeProfile} votes={this.state.votes} />
+        <ProfileBox profileClass={this.state.profileClass} profile={this.state.profile} closeProfile={this.closeProfile} votes={voteList} onBillSearchChange={this.onBillSearchChange} retrievingVotes={this.state.retrievingVotes} />
       </div>
     );
   },
   fetchJSON: function(path, type) {
-    console.log('fetch JSON')
     var request = new XMLHttpRequest();
     request.open('GET', path, true);
     request.onload = function() {
       if (request.status >= 200 && request.status < 400) {
         // Success!
         var data = JSON.parse(request.responseText);
-        console.log('requestin');
-        console.log(data);
         if (type == 'politicians') {
           this.setState({politicians: data['results']});
         }
         else if (type == 'votes') {
-          this.setState({votes: data['results']});
+          this.setState({votes: data['results'],
+                          retrievingVotes: false
+                        });
         }
         else {
           console.log('type not politician or votes');
@@ -118,13 +135,11 @@ var AppBox = React.createClass({
     request.send();
   },
   getPoliticianVotes: function(id) {
-    console.log("getVotes");
     url = '/pol/' + id;
     this.fetchJSON(url, 'votes');
   },
   componentDidMount: function() {
     // temporary measure - is supposed to read from file
-    console.log('did mount');
     url = '/initialize';
     this.fetchJSON(url, 'politicians');
   },
@@ -133,16 +148,22 @@ var ProfileBox = React.createClass({
   render: function() {
     var classes = 'profileBox ' + this.props.profileClass;
     var closeClass = 'close ' + this.props.profileClass;
+    if (!this.props.profile.party_slug) {
+      var partyName = this.props.profile.party_name;
+    }
+    else {
+      var partyName = this.props.profile.party_slug;
+    }
     return (
       <div className={classes}>
         <div className="profile">
           <a className={closeClass} onClick={this.props.closeProfile} href="#"></a>
-          <h2>{this.props.profile.name}</h2>
-          <div>Basic profile info.</div>
-          <div>Search interface.</div>
+          <h2 className="name">{this.props.profile.name}</h2>
+          <span className="info"><h3 className="riding">{this.props.profile.riding}</h3><h3 className="party">{partyName}</h3></span>
+          <BillSearch onBillSearchChange={this.props.onBillSearchChange} />
         </div>
         <div className="votes">
-          <BillStack votes={this.props.votes} />
+          <BillStack votes={this.props.votes} retrievingVotes={this.props.retrievingVotes} />
         </div>
       </div>
     );
@@ -150,59 +171,119 @@ var ProfileBox = React.createClass({
 });
 var BillStack = React.createClass({
   render: function() {
-    var objectNodes = this.props.votes.map(function (object, i) {
-      if (object.vote == 'Y') {
-        voteClass = voteText = 'yes ';
-      }
-      else if (object.vote == 'N') {
-        voteClass = voteText = 'no ';
+    console.log(this.props.votes == true);
+    if (this.props.votes.length  > 0) {
+      console.log('truly yours');
+      var objectNodes = this.props.votes.map(function (object, i) {
+        if (object.vote == 'Y') {
+          voteClass = voteText = 'yes ';
+        }
+        else if (object.vote == 'N') {
+          voteClass = voteText = 'no ';
+        }
+        else {
+          voteClass = '';
+          voteText = 'no vote';
+          console.log('no vote');
+        }
+        voteClass += 'vote';
+        var lawText = object.law ? 'passed' : 'failed';
+        lawClass = 'law ' + lawText;
+        if (object.short_title_en) {
+          name = object.short_title_en;
+        }
+        else {
+          name = object.name_en;
+        }
+        return (
+          <tr key={i}>
+            <td></td>
+            <td className="number">{object.number}</td>
+            <td className={voteClass}>{voteText}</td>
+            <td className="shortname">{name}</td>
+            <td className={lawClass}>{lawText}</td>
+            <td></td>
+          </tr>
+        );
+      }.bind(this));
+      return (
+        <div className='stickyHelper'>
+          <table className='billStack'>
+            <thead>
+              <tr>
+                <th></th>
+                <th className="number">Number</th>
+                <th className="vote">Vote</th>
+                <th className="shortname">Name</th>
+                <th className="law">Law</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              {objectNodes}
+            </tbody>
+          </table>
+        </div>
+      );
+    }
+    else {
+      console.log('falsely accused');
+      if (this.props.retrievingVotes) {
+        var loader = <div className="loader"></div>;
       }
       else {
-        voteClass = '';
-        voteText = 'no vote';
-        console.log('no vote');
-      }
-      voteClass += 'vote';
-      var lawText = object.law ? 'passed' : 'failed';
-      lawClass = 'law ' + lawText;
-      if (object.short_title_en) {
-        name = object.short_title_en;
-      }
-      else {
-        name = object.name_en;
+        var loader = <div className="no-results">No Results</div>;
       }
       return (
-        <tr key={i}>
-          <td className="number">{object.number}</td>
-          <td className={voteClass}>{voteText}</td>
-          <td className="shortname">{name}</td>
-          <td className={lawClass}>{lawText}</td>
-        </tr>
-      );
-    }.bind(this));
-    return (
-      <div className='stickyHelper'>
-        <table className='billStack'>
-          <thead>
-            <tr>
-              <th className="number">Number</th>
-              <th className="vote">Vote</th>
-              <th className="shortname">Name</th>
-              <th className="law">Law</th>
+        <div className='stickyHelper'>
+          <table className='billStack'>
+            <thead>
+              <tr>
+                <th></th>
+                <th className="number">Number</th>
+                <th className="vote">Vote</th>
+                <th className="shortname">Name</th>
+                <th className="law">Law</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr className="empty">
+              <td colspan=""></td>
+              <td className="number">empty</td>
+              <td className="vote">empty</td>
+              <td className="shortname">empty</td>
+              <td className="law">empty</td>
+              <td></td>
             </tr>
-          </thead>
-          <tbody>
-            {objectNodes}
-          </tbody>
-        </table>
+            </tbody>
+          </table>
+          {loader}
+        </div>
+      );
+        
+    }
+    
+  }
+});
+var BillSearch = React.createClass({
+  render: function() {
+    return (
+      <div className="billSearch">
+        <form>
+          <input type="search" placeholder="Search bills by name or number..." onChange={this.props.onBillSearchChange} />
+        </form>
       </div>
+      
     );
   }
 });
 var SearchBox = React.createClass({
   render: function() {
     var classes = 'searchBox ' + this.props.searchClass;
+    var containerclasses = 'searchBox-noscroll ' + this.props.searchClass;
     return (
+      <div className={containerclasses}>
       <div className={classes}>
         <form>
           <input type="search" placeholder="Search..." onChange={this.props.onSearchChange} />
@@ -212,6 +293,7 @@ var SearchBox = React.createClass({
           <SearchStack changeClass={this.props.changeClass} politicians={this.props.politicians} profile={this.props.profile} />
         </div>
       </div>
+      </div>
     );
   }
 });
@@ -219,16 +301,7 @@ var SearchStack = React.createClass({
   render: function() {
     classString = "searchStack";
     var currentProfileID = this.props.profile.id;
-    console.log(currentProfileID);
-    var boundClick = this.props.changeClass;
-    var politicians = this.props.politicians;
-      if (currentProfileID) {
-        for (var i = 0; i < this.props.politicians.length; i++) {
-          if (this.props.politicians[i].id === this.props.profile.id) {
-            politicians.splice(0, 0, politicians.splice(i, 1)[0]);
-          }
-        }
-      }      
+    var boundClick = this.props.changeClass;   
     var objectNodes = this.props.politicians.map(function (object, i) {
       var imgURL = "url('http://104.236.172.89/static/images/" + object.imgurl + "')";
       var classString = '';
@@ -253,7 +326,6 @@ var SearchStack = React.createClass({
         </a>
       );
     }.bind(this));
-    console.log(objectNodes);
     return (
       <div className={classString}>
         <h2>Members of Parliament</h2>
