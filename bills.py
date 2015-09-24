775e3ef96c41be5331807a480ab5b381ecc9b6c6
Shay Neufeld <shayneufeld@gmail.com>
2015-09-24 16:06:16 -0400

    not working yet - trying to count the votes.

--- bills.py
+++ bills.py
@@ -28,21 +28,12 @@ def get_bill_json(votequestion_id):
     cursor = get_cursor()
 
     query = (
-        "SELECT id,number,name_en,law,short_title_en,status_code "
-        "FROM bills_bill "
-        "WHERE id = "
-        "("
-            "SELECT bill_id "
-            "FROM bills_votequestion "
-            "WHERE id = (%s)"
-        ")"
-    )
-
-    query = (
-        "SELECT c.name,b.vote "
-        "FROM core_party c, bills_partyvote b "
-        "WHERE b.votequestion_id = (%s)"
-        "AND b.party_id = c.id"
+        "SELECT b.vote, c.party_id, p.name "
+        "FROM bills_membervote b, core_electedmember c, core_party p "
+        "WHERE b.politician_id = c.politician_id "
+        "AND c.party_id = p.id "
+        "AND b.votequestion_id = (%s) "
+        "ORDER BY c.party_id "
     )
 
     # If any parameters are used in the above query, insert them in the parameters tuple
@@ -55,6 +46,26 @@ def get_bill_json(votequestion_id):
 
     # Fetch results
     bill_results = cursor.fetchall()
+
+    cy,cn,ca = 0,0,0
+    ny,nn,na = 0,0,0
+    ly,ln,la = 0,0,0
+    by,bn,ba = 0,0,0
+    gy,gn,ga = 0,0,0
+
+    partyvotes = [[0 for x in range(30) for x in range(3)]
+
+    for i in range(0,len(bill_results)):
+        party_col = bill_results[i]['party_id']
+        if bill_results[i]['vote'] == 'Y':
+                partyvotes[party_col][0] += 1
+        if bill_results[i]['vote'] == 'N':
+                partyvotes[party_col][1] += 1
+        if bill_results[i]['vote'] == 'A':
+                partyvotes[party_col][2] += 1
+
+
+    print  partyvotes
 
     # return a JSON response to React (includes header, no extra work needed)
     return jsonify(results=bill_results)
