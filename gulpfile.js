41823581970e1db21a045b3f4b041e3f13a11928
Braden Neufeld <bneufeld@hmc.edu>
2015-10-03 19:25:00 -0700

    gulp fixes for separate react component files

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,8 @@
 var gulp = require('gulp');
-var browserify = require('gulp-browserify');
+var source = require('vinyl-source-stream');
+var browserify = require('browserify');
+var watchify = require('watchify');
+var reactify = require('reactify');
 var size = require('gulp-size');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
@@ -7,15 +10,30 @@ var sass = require('gulp-sass');
 var image = require('gulp-image');
 var del = require('del');
 var preprocess = require('gulp-preprocess');
-
-gulp.task('javascript', function () {
-    gulp.src('src/js/parle.js')
-        .pipe(gulp.dest('static/js'))
-        .pipe(size());
-});
+var debug = require('gulp-debug');
 
 gulp.task('watch_javascript', function() {
-  gulp.watch('src/js/parle.js', ['javascript']);
+    var bundler = browserify({
+        entries: ['./src/js/parle.js'], // Only need initial file, browserify finds the deps
+        transform: [reactify], // We want to convert JSX to normal javascript
+        debug: true, // Gives us sourcemapping
+        cache: {}, packageCache: {}, fullPaths: true // Requirement of watchify
+    });
+    var watcher  = watchify(bundler);
+
+    return watcher
+    .on('update', function () { // When any files update
+        var updateStart = Date.now();
+        console.log('Updating!');
+        watcher.bundle() // Create new bundle that uses the cache for high performance
+        .pipe(source('parle.js'))
+    // This is where you add uglifying etc.
+        .pipe(gulp.dest('./static/js/'));
+        console.log('Updated!', (Date.now() - updateStart) + 'ms');
+    })
+    .bundle() // Create the initial bundle when starting the task
+    .pipe(source('parle.js'))
+    .pipe(gulp.dest('./static/js/'));
 });
 
 gulp.task('sass_me', function () {
